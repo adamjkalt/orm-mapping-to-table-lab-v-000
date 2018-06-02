b4fac2c94e96b99a9b4afa275cb538355e34857b
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-02 03:24:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,15 +9,4 @@ class Student
     @grade = grade
   end
 
-  def self.create_table
-    sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        album TEXT
-        )
-        SQL
-    DB[:conn].execute(sql)
-  end
-
 end
